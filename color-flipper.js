036912c9315475e3a479ba1142e96f295525b94c
Dhevine <67029282+Dhevine@users.noreply.github.com>
2022-04-21 13:25:49 +0100

    fix

--- color-flipper.js
+++ color-flipper.js
@@ -6,35 +6,35 @@ body.style.backgroundColor = "rgb(0,233,123)"
 // console.dir(body.style)
 
 
-// another idea
-// const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
+another idea
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
 
-// clickBtn.addEventListener("click", function () {
-//   let hexCode = "#"
-//   for (let i = 0; i < 6; i++) {
-//     hexCode += hex[randomNumber()]
-//   }
-//   console.log(hexCode);
-// })
-
-// function randomNumber() {
-//   return Math.floor(Math.random() * hex.length)
-// }
-
-
-
-// my idea
 clickBtn.addEventListener("click", function () {
-  const ree = ["a", 1, "b", 2, "c", 3, "d", 4, "e", 5, 6, "f", 7, 8, 9]
-  const rand1 = Math.trunc(Math.random() * ree.length)
-  const rand2 = Math.trunc(Math.random() * ree.length + 1)
-  const deleteNo = ree.splice(rand1, rand2)
-  const newArr = [...deleteNo, ...ree].slice(4, 10).join("")
-  const hexCode = `#${newArr}`;
-  // console.log(hexCode);
-  body.style.backgroundColor = hexCode
-  hexNumber.textContent = hexCode
-  rgbNumber.textContent = body.style.backgroundColor
+  let hexCode = "#"
+  for (let i = 0; i < 6; i++) {
+    hexCode += hex[randomNumber()]
+  }
+  console.log(hexCode);
 })
+
+function randomNumber() {
+  return Math.floor(Math.random() * hex.length)
+}
+
+
+
+// another idea
+// clickBtn.addEventListener("click", function () {
+//   const ree = ["a", 1, "b", 2, "c", 3, "d", 4, "e", 5, 6, "f", 7, 8, 9]
+//   const rand1 = Math.trunc(Math.random() * ree.length)
+//   const rand2 = Math.trunc(Math.random() * ree.length + 1)
+//   const deleteNo = ree.splice(rand1, rand2)
+//   const newArr = [...deleteNo, ...ree].slice(4, 10).join("")
+//   const hexCode = `#${newArr}`;
+//   // console.log(hexCode);
+//   body.style.backgroundColor = hexCode
+//   hexNumber.textContent = hexCode
+//   rgbNumber.textContent = body.style.backgroundColor
+// })
 
 
