fbb3c870d1ccfd7eec045467a2e564e5a32350f9
divine <divineobi07@gmail.com>
2022-05-13 22:32:41 +0100

    display colors on screen

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const clickBtn = document.querySelector("button")
-const hexNumber = document.querySelector("#hex")
-const rgbNumber = document.querySelector("#rgb")
-const body = document.querySelector("body")
-body.style.backgroundColor = "rgb(0,233,123)"
+const clickBtn = document.querySelector("button");
+const hexNumber = document.querySelector("#hex");
+const rgbNumber = document.querySelector("#rgb");
+const body = document.querySelector("body");
+body.style.backgroundColor = "rgb(0,233,123)";
 
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 clickBtn.addEventListener("click", function () {
-  let hexCode = "#"
+  let hexCode = "#";
   for (let i = 0; i < 6; i++) {
-    hexCode += hex[randomNumber()]
+    hexCode += hex[randomNumber()];
   }
-  console.log(hexCode);
-})
+  body.style.backgroundColor = hexCode;
+  hexNumber.textContent = hexCode;
+  rgbNumber.textContent = body.style.backgroundColor;
+});
 
 function randomNumber() {
-  return Math.floor(Math.random() * hex.length)
+  return Math.floor(Math.random() * hex.length);
 }
-
-
 
 // another idea
 // clickBtn.addEventListener("click", function () {
@@ -33,5 +33,3 @@ function randomNumber() {
 //   hexNumber.textContent = hexCode
 //   rgbNumber.textContent = body.style.backgroundColor
 // })
-
-
